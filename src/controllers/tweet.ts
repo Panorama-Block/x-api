37db6a82a9ff7f4c79ff8328f2a451d3b9e80a3a
felipesaadi <felipesaadisiegert@gmail.com>
2025-02-01 17:52:33 -0300

    feat: add tweets pagination

--- src/controllers/tweet.ts
+++ src/controllers/tweet.ts
@@ -2,14 +2,17 @@ import { RequestHandler } from "express"
 import * as tweetService from '../services/tweet'
 
 export const getAllTweets: RequestHandler = async (req, res) => {
-  const tweets = await tweetService.getTweets()
+  const page = parseInt(req.query.page as string) || 1;
+  const limit = parseInt(req.query.limit as string) || 20;
 
-  if (tweets) {
-    res.status(200).json({ ok: tweets })
+  const result = await tweetService.getTweets({ page, limit });
+
+  if (result) {
+    res.status(200).json(result);
   }
   else {
     res.status(500).json({
       error: 'An error occurred in the request'
-    })
+    });
   }
 }
--- src/services/tweet.ts
+++ src/services/tweet.ts
@@ -1,11 +1,33 @@
 import Tweet from "../model/tweet"
 
-export const getTweets = async () => {
+interface PaginationOptions {
+  page: number;
+  limit: number;
+}
+
+export const getTweets = async (options: PaginationOptions = { page: 1, limit: 20 }) => {
   try {
-    const tweets = await Tweet.find().sort({ created_at_datetime: -1 })
-    return tweets
+    const skip = (options.page - 1) * options.limit;
+
+    const [tweets, total] = await Promise.all([
+      Tweet.find()
+        .sort({ created_at_datetime: -1 })
+        .skip(skip)
+        .limit(options.limit),
+      Tweet.countDocuments()
+    ]);
+
+    return {
+      tweets,
+      pagination: {
+        total,
+        page: options.page,
+        limit: options.limit,
+        totalPages: Math.ceil(total / options.limit)
+      }
+    };
   }
   catch (error) {
-    return false
+    return false;
   }
 }
